8b1c84f9af337f8c0bebaa950f2cf3060b3077fe
Noal Balint <noalbalint@gmail.com>
2024-01-25 11:54:28 +0100

    add buttons for show/hide cc + bcc

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{ label: string }>()
+</script>
+
+<style scoped>
+button {
+  outline: none;
+  border: 2px solid #76B39D;
+  opacity: 0.5;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  min-width: 4rem;
+}
+
+button:hover {
+  background-color: #76B39D;
+}
+</style>
--- src/components/CreateEmail.vue
+++ src/components/CreateEmail.vue
@@ -1,8 +1,27 @@
 <template>
   <!-- TODO: find a formatter that will wrap these automatically like the attributes -->
-  <div class="w-full border-solid border-black border-2 border-b-0 text-left min-h-[90vh] mt-[10vh] flex flex-col">
-    <Input label="To:" />
-    <TextArea label="Cc:" />
+  <div class="w-full border-solid border-black border-2 text-left min-h-[90vh] my-[5vh] flex flex-col">
+    <section class="flex items-center">
+      <Input label="To:" />
+      <Button
+        class="m-2"
+        label="Cc:"
+        @click="showCc = !showCc"
+      />
+      <Button
+        class="m-2"
+        label="Bcc:"
+        @click="showBcc = !showBcc"
+      />
+    </section>
+    <TextArea
+      v-if="showCc"
+      label="Cc:"
+    />
+    <TextArea
+      v-if="showBcc"
+      label="Bcc:"
+    />
     <Input label="Subject:" />
     <hr>
     <TextArea
@@ -15,4 +34,9 @@
 <script setup lang="ts">
 import Input from './Input.vue';
 import TextArea from './TextArea.vue';
+import Button from './Button.vue'
+import { ref } from 'vue';
+
+let showCc = ref(false);
+let showBcc = ref(false);
 </script>
