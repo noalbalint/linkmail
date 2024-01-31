106405ffbfeabb1b1dcd0ca41e17770243dffed3
Noal Balint <noalbalint@gmail.com>
2024-01-31 15:11:38 +0100

    fix consume page background color

--- src/pages/Consume.vue
+++ src/pages/Consume.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center justify-center">
     <span class="text-2xl pb-2"> Opening default email client... </span>
     <span v-if="timeoutComplete"> No client not found! :( </span>
   </div>
