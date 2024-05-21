9e8adee5015b317dfcb616cf4bd430f706127c58
Noal Balint <noalbalint@gmail.com>
2024-05-21 22:31:21 +0100

    add beta banner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div class="flex h-full min-h-screen flex-col items-center">
     <NavBar />
     <router-view class="p-4 max-w-7xl py-8" />
+    <span class="beta-banner">beta preview - <a href="https://linkmail.co/consume?mailcode=6x4ChBKrQ9pXzcVudSQe">your
+        feedback</a> is apprecaited!!</span>
   </div>
 </template>
 
@@ -11,3 +13,14 @@
 >
 import NavBar from './components/NavBar.vue';
 </script>
+
+<style scoped>
+.beta-banner {
+  position: fixed;
+  background-color: yellow;
+  width: 100%;
+  text-align: center;
+  bottom: 0;
+  font-weight: 600;
+}
+</style>
