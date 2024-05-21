8fe91fd11631385814a8b6b61fbc205f07567779
Noal Balint <noalbalint@gmail.com>
2024-05-21 22:37:17 +0100

    send email in new tab

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="flex h-full min-h-screen flex-col items-center">
     <NavBar />
     <router-view class="p-4 max-w-7xl py-8" />
-    <span class="beta-banner">beta preview - <a href="https://linkmail.co/consume?mailcode=6x4ChBKrQ9pXzcVudSQe">your
+    <span class="beta-banner">beta preview - <a
+        target="_blank"
+        href="https://linkmail.co/consume?mailcode=6x4ChBKrQ9pXzcVudSQe"
+      >your
         feedback</a> is apprecaited!!</span>
   </div>
 </template>
