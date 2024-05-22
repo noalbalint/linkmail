528df094e5978fe75d8d9b1b91342fd2c9b1f49b
Noal Balint <noalbalint@gmail.com>
2024-05-22 12:30:07 +0100

    add more context on Demo page

--- src/pages/Demo.vue
+++ src/pages/Demo.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="max-w-2xl">
 
-    <p class="text-xl underline text-center pb-4 lg:pb-8 pt-6"> The Problem: </p>
+    <p class="mt-6 px-5 lg:px-0"> Linkmail is an <a
+        target="_blank"
+        href="https://github.com/noalbalint/linkmail"
+      >opensource project</a> for accelerating sociopolitical movements. It creates a double efficiency: enabling
+      activists to more easily write and share email templates, and enabling audiences to more easily send them.
+      <br>
+      <br>
+      Our service is particularly useful for social media activists who don't have the capacity/budget for
+      a website, let alone a dedicated email engine like <a
+        href="https://www.newmode.net/home"
+        target="_blank"
+      >newmode</a>. However, larger organizations will also benefit from our low-friction technology.
+      <br>
+      <br>
+      We also hope to encourage more people to become micro-campaigners.
+      Political offices put more weight on individual/unique emails, so <b> a large number
+        of individuals writing emails for their peers is more influential than a handful of NGOs writing emails
+        for thousands of members. </b> This is our vision for Linkmail.
+    </p>
 
-    <div class="flex flex-col-reverse items-center lg:flex-row">
+    <section class="flex flex-col-reverse items-center lg:flex-row pt-6">
       <video
         controls
         preload="auto"
@@ -13,7 +31,7 @@
       />
       <div class="pl-5 flex flex-col py-2">
         <span>
-          Email campaigns on Instagram have too much cognitive load, resulting in abandoned emails. Users must:
+          The current flow for a standard social media email drive is some derivative of the following:
         </span>
         <ul class="pl-5 list-decimal py-8">
           <li>Click link in account bio</li>
@@ -26,18 +44,15 @@
           <li>Copy recipient email address(es)</li>
           <li>Switch to mail app + paste</li>
           <li>[[ Repeat as necessary ]]</li>
-          <li>Send! ... if they get that far</li>
         </ul>
         <span class="pb-4 lg:pb-0">
           In the dopamine-saturated instant-gratification environment of Instagram, for most users this is simply too
-          much.
+          many steps and they abandon their message without sending it.
         </span>
       </div>
-    </div>
+    </section>
 
-    <p class="text-xl underline text-center pb-4 lg:pb-8 pt-6">The Solution: </p>
-
-    <div class="flex flex-col-reverse items-center lg:flex-row">
+    <section class="lg:h-[456px] flex flex-col-reverse items-center lg:flex-row pt-6">
       <video
         controls
         preload="auto"
@@ -45,10 +60,13 @@
         :src="video2"
         style="max-width: 200px;"
       />
-      <div class="pl-5 flex flex-col py-2">
+      <div class="pl-5 flex flex-col py-2 h-full">
         <span>
-          Linkmail vastly simplifies this flow by compressing more information into the link being shared.
-          Now all users have to do is:
+          <a
+            href="linkmail.co/create"
+            target="_blank"
+          >Linkmail</a> vastly simplifies this by compressing more information into the link being shared.
+          The new flow is:
         </span>
         <ul class="pl-5 py-4 list-decimal">
           <li>Click link</li>
@@ -56,16 +74,16 @@
           <li>Send!</li>
         </ul>
         <span>
-          This makes all the difference – when it's easier to take action, more people do!
+          This minor improvement makes a huge the difference – when it's easier to take action, more people do!
         </span>
         <br>
         <div class="h-full" />
         <span class="pb-4 lg:pb-0">
-          NOTE: in both demos, you'll see that I must slightly reformatt the email content. This is due to a
+          NOTE: in both demos, you'll see that I must slightly reformat the email content. This is due to a
           problem with my phone specifically, and most users won't experience this.
         </span>
       </div>
-    </div>
+    </section>
   </div>
 
   <RouterLink
