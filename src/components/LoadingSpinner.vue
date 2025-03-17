891cc0684d752cb901f27e83a998880b31ccbeb0
Noal Balint <noalbalint@gmail.com>
2025-03-16 21:21:09 -0700

    loading spinner color

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -7,7 +7,7 @@
   width: 40px;
   height: 40px;
   border: 4px solid #ccc;
-  border-top: 4px solid #4713f3;
+  border-top: 4px solid #8fbc8f;
   border-radius: 50%;
   animation: spin 1s linear infinite;
 }
